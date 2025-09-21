3618a33c1be6066befde3e8f21a91b9043ab8f98
kurk6455 <kurk6455@gmail.com>
2025-09-21 21:26:54 +0530

    step 2: adoption form data

--- beginner/petAdoption/src/App.jsx
+++ beginner/petAdoption/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -8,8 +8,10 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />} />
-          <Route path="/submit" element={<AdoptionTable />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<AdoptionForm />} />
+            <Route path="/submit" element={<AdoptionTable />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
@@ -17,10 +19,11 @@ function App() {
 }
 
 function Layout() {
+
   return (
     <>
       <Header />
-      <AdoptionForm />
+      <Outlet />
     </>
   )
 }
@@ -36,33 +39,54 @@ function Header() {
 }
 
 function AdoptionForm() {
+  const [details, setDetails] = useState([]);
 
-  function submitFn(){
+  const petName = useRef();
+  const petType = useRef();
+  const breed = useRef();
+  const name = useRef();
+  const email = useRef();
+  const phone = useRef();
 
+  function submitFn() {
+    //it's asynchronous
+    setDetails([...details, {
+      petName : petName.current.value,
+      petType : petType.current.value,
+      breed : breed.current.value,
+      name : name.current.value,
+      email : email.current.value,
+      phone : phone.current.value,
+    }]);
   }
+
+  //Print the detial only when details is updated
+  useEffect(function(){
+    console.log(details);
+  }, [details]);
 
   return (
     <>
-      <label>Pet Name</label><br/>
-      <input type="text" placeholder='Pet Name...'/><br/>
-      <label>Pet Type</label><br/>
-      <input type="text" placeholder='Pet Type...'/><br/>
-      <label>Breed</label><br/>
-      <input type="text" placeholder='Breed...'/><br/>
-      <label>Your Name</label><br/>
-      <input type="text" placeholder='Your Name...'/><br/>
-      <label>Email</label><br/>
-      <input type="text" placeholder='Email...'/><br/>
-      <label>Phone</label><br/>
-      <input type="text" placeholder='Phone...'/><br/>
-      <Link onClick={submitFn} to="/submit">Submit</Link>
+      <label>Pet Name</label><br />
+      <input ref={petName} type="text" placeholder='Pet Name...' /><br />
+      <label>Pet Type</label><br />
+      <input ref={petType} type="text" placeholder='Pet Type...' /><br />
+      <label>Breed</label><br />
+      <input ref={breed} type="text" placeholder='Breed...' /><br />
+      <label>Your Name</label><br />
+      <input ref={name} type="text" placeholder='Your Name...' /><br />
+      <label>Email</label><br />
+      <input ref={email} type="text" placeholder='Email...' /><br />
+      <label>Phone</label><br />
+      <input ref={phone} type="text" placeholder='Phone...' /><br />
+      <Link onClick={submitFn} to='/submit'>Submit</Link>
     </>
   )
 }
 
-function AdoptionTable(){
+function AdoptionTable() {
 
-  return(
+  return (
     <>
       <div>Inside adoption table</div>
     </>
